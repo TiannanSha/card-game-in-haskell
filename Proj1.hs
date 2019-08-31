400e6ac85e560745fda7f1382d14cf9d5c6db6cf
Tiannan Sha <tiannans@student.unimelb.edu.au>
2019-08-31 14:36:20 +1000

    implement the three eq parts of the feedback function

--- Proj1.hs
+++ Proj1.hs
@@ -9,23 +9,38 @@ module Proj1 (feedback, initialGuess, nextGuess, GameState) where
 import Card
 import Data.List
 
--------type synonyms------- 
+----------------------type synonyms------------------------ 
 
-type Answer = [Card]
-type Guess = [Card]
+type AnswerCards = [Card]
+type GuessCards = [Card]
 type Feedback = (Int,Int,Int,Int,Int)	
 -- GameState stores number of cards in the answer and all the valid guesses
-data GameState = GameState Int [Guess] 
+data GameState = GameState Int [GuessCards] 
 
 
+-- returns (numEqCard, numLsRank, numEqRank, numGtRank, numEqSuit)
+feedback :: AnswerCards -> GuessCards -> Feedback
+feedback as gs = (numEq as gs, 0, numEq (map getRank as) (map getRank gs),0,numEq (map getSuit as) (map getSuit gs))
 
-feedback :: Answer -> Guess -> Feedback
-feedback _ _ = (0,0,0,0,0)
+-- this function takes an Answer (a list of cards) and a Guess (a list of cards) and returns the number of cards that are same in both lists
+numEq :: Eq a => [a] -> [a] -> Int
+numEq as gs = length as - length (as\\gs)  -- as\\gs delete one ocurrence of every element in gs
 
-initialGuess :: Int -> (Guess, GameState)
+--numRankEq :: AnswerCards -> GuessCards -> Int
+--numRankEq as gs = legnth as - length (deleteFirstsBy as gs)
+
+getSuit :: Card -> Suit
+getSuit (Card s _) = s
+
+getRank :: Card -> Rank
+getRank (Card _ r) = r
+
+---------------------------------------------------------------
+
+initialGuess :: Int -> (GuessCards, GameState)
 initialGuess _ = ([Card Club R2, Card Heart Ace], 
 	(GameState 2 [[Card Club R2, Card Heart Ace], [Card Club R3, Card Heart Ace]]))
 
-nextGuess :: (Guess,GameState) -> Feedback -> (Guess,GameState)
+nextGuess :: (GuessCards,GameState) -> Feedback -> (GuessCards,GameState)
 nextGuess _ _ = ([Card Club R2, Card Heart Ace], 
 	(GameState 2 [[Card Club R2, Card Heart Ace], [Card Club R3, Card Heart Ace]]))
