215c9e9549d8941291f61c92314dcd1cf6d15f9b
Tiannan Sha <tiannans@student.unimelb.edu.au>
2019-09-01 20:35:35 +1000

    finish 2,3,4 cards guessing with only choosing consistent guesses

--- Proj1.hs
+++ Proj1.hs
@@ -1,5 +1,5 @@
 --  File     : Card.hs
---  Author   : Tiannan Sha
+--  Author   : Tiannan Sha <tiannans@student.unimelb.edu.au>
 --  Purpose  : An implementation of answerer and guesser functions
 
 -- | This code ... blah blah
@@ -13,26 +13,26 @@ import Data.List
 
 type AnswerCards = [Card]
 type GuessCards = [Card]
-type Feedback = (Int,Int,Int,Int,Int)	
+type Feedback = (Int,Int,Int,Int,Int)
 
 -- GameState stores number of cards in the answer and all the valid guesses
-data GameState = GameState Int [Feedback] [GuessCards] [GuessCards] 
+data GameState = GameState [Feedback] [GuessCards] [GuessCards] 
 
 ---------------------------------------------------------------
 
 -- returns (numEqCard, numLsRank, numEqRank, numGtRank, numEqSuit)
 feedback :: AnswerCards -> GuessCards -> Feedback
 feedback as gs = 
-	(numEq as gs, 
-	length (filter (< minimum gRanks) aRanks), 
-	numEq aRanks gRanks,
-	length (filter (> maximum gRanks) aRanks),
-	numEq aSuits gSuits)
-	where 
-		aRanks = map getRank as
-		gRanks = map getRank gs
-		aSuits = map getSuit as
-		gSuits = map getSuit gs
+  (numEq as gs, 
+  length (filter (< minimum gRanks) aRanks), 
+  numEq aRanks gRanks,
+  length (filter (> maximum gRanks) aRanks),
+  numEq aSuits gSuits)
+    where 
+      aRanks = map getRank as
+      gRanks = map getRank gs
+      aSuits = map getSuit as
+      gSuits = map getSuit gs
 
 -- this function takes two lists and return number of shared elements in a 
 -- one-to-one fashion. that is, numEq [1,1,1] [1,1] = 2
@@ -50,30 +50,29 @@ getRank (Card _ r) = r
 --inital guess' ranks should evenly divide the rank space
 initialGuess :: Int -> (GuessCards, GameState)
 initialGuess 2 
-	= (
-		[Card Club R6, Card Heart Jack], 
-		GameState 2 [] [] (enumAll 2)
-	  )
+  = ([Card Club R6, Card Heart Jack], GameState [] [] (enumAll 2))
+initialGuess 3
+  = ([Card Club R4, Card Heart R8, Card Spade Queen], GameState [] [] (enumAll 3))
+initialGuess 4
+  = ([Card Club R4, Card Diamond R7, Card Heart R10, Card Spade Queen], GameState [] [] (enumAll 4))
 
 -- first add the latest Feedback to the game state
 -- for all the possible guesses in the old game state,
 -- check its consistency against all the past [GuessCards] and [Feedback]
 -- add a guess to new gameState iff it's consistent
 nextGuess :: (GuessCards,GameState) -> Feedback -> (GuessCards,GameState)
-nextGuess (gs, GameState num fs pastGss posGss) f 
-	= let 
-		newFs = f:fs
-		newPastGss = gs:pastGss
-		newPosGss = [posGs | posGs<-posGss, isConsistent posGs newFs newPastGss]
-	  in 
-	  	(head newPosGss, GameState num newFs newPastGss newPosGss)
+nextGuess (gs, GameState fs pastGss posGss) f 
+  = let 
+      newFs = f:fs
+      newPastGss = gs:pastGss
+      newPosGss = [posGs | posGs<-posGss, isConsistent posGs newFs newPastGss]
+    in 
+      (head newPosGss, GameState newFs newPastGss newPosGss)
+-- TODO: use a smart way to pick a guess among newPosGss
 
 isConsistent :: GuessCards->[Feedback]->[GuessCards]->Bool
 isConsistent posGs fs pastGss = 
-	and [feedback posGs pastGs == f | (f,pastGs)<-zip fs pastGss]
-
---_isConsistent :: GuessCards->GuessCards->Feedback->Bool
---_isConsistent oldGs posGs f = f==feedback posGs oldGs
+  and [feedback posGs pastGs == f | (f,pastGs)<-zip fs pastGss]
 
 -- this function takes the number of cards in one guess 
 -- returns all the possible guesses as list of guesses
@@ -82,15 +81,15 @@ isConsistent posGs fs pastGss =
 -- test correctness: length (enumAll 4) = 270725
 enumAll :: Int -> [GuessCards]
 enumAll x
-	| x==2 = [[c1,c2] | c1<-[(Card Club R2)..(Card Spade King)], 
-						c2<-[succ(c1)..(Card Spade Ace)]
-			 ]
-	| x==3 = [[c1,c2,c3] | c1<-[(Card Club R2)..(Card Spade Queen)],
-						   c2<-[succ(c1)..(Card Spade King)],
-						   c3<-[succ(c2)..(Card Spade Ace)]
-			 ]
-	| x==4 = [[c1,c2,c3,c4] | c1<-[(Card Club R2)..(Card Spade Jack)],
-						   c2<-[succ(c1)..(Card Spade Queen)],
-						   c3<-[succ(c2)..(Card Spade King)],
-						   c4<-[succ(c3)..(Card Spade Ace)]
-			 ]
+  | x==2 = [[c1,c2] | c1<-[(Card Club R2)..(Card Spade King)], 
+                      c2<-[succ(c1)..(Card Spade Ace)]
+           ]
+  | x==3 = [[c1,c2,c3] | c1<-[(Card Club R2)..(Card Spade Queen)],
+                         c2<-[succ(c1)..(Card Spade King)],
+                         c3<-[succ(c2)..(Card Spade Ace)]
+           ]
+  | x==4 = [[c1,c2,c3,c4] | c1<-[(Card Club R2)..(Card Spade Jack)],
+                            c2<-[succ(c1)..(Card Spade Queen)],
+                            c3<-[succ(c2)..(Card Spade King)],
+                            c4<-[succ(c3)..(Card Spade Ace)]
+           ]
